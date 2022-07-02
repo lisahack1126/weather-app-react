9d2024766c5dff3148fe3f7c998814729ae1df21
lisahack1126 <lisa.weddington1126@gmail.com>
2022-07-02 13:21:43 -0400

    Remove unneeded brackets

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -26,16 +26,16 @@ export default function FormattedDate(props) {
   ];
 
   let day = days[props.date.getDay()];
-  let date = [props.date.getDate()];
-  let hours = props.date.getHours();
-  let minutes = props.date.getMinutes();
   let month = months[props.date.getMonth()];
+  let date = props.date.getDate();
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
-  }
-  if (hours < 10) {
-    hours = `0${hours}`;
   }
   let ampm = hours >= 12 ? `pm` : `am`;
   hours = hours % 12;
